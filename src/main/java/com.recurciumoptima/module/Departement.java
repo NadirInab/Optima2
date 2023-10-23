5e08f122fecda1c5a9b8e474c7de0b06c4a6689a
nadir inab <nadir.inab.dev@gmail.com>
2023-10-23 22:40:10 +0100

    module employee and departement and task are well added

---
+++ src/main/java/com.recurciumoptima/module/Departement.java
@@ -0,0 +1,62 @@
+package com.recurciumoptima.module;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "departement")
+public class Departement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="id")
+    private int id;
+    private String name;
+    private String description;
+//    @OneToMany
+//    private List<Employee> employees;
+
+    public Departement(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Departement() {
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public List<Employee> getEmployees() {
+//        return employees;
+//    }
+//
+//    public void setEmployees(List<Employee> employees) {
+//        this.employees = employees;
+//    }
+
+    @Override
+    public String toString() {
+        return "Departement{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com.recurciumoptima/module/Task.java
@@ -0,0 +1,100 @@
+package com.recurciumoptima.module;
+
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private Date limitDate;
+    private String priority;
+    @ManyToOne
+    private Employee employee;
+    private String status;
+
+    public Task(String name, String description, Date limitDate,String priority) {
+        this.name = name;
+        this.description = description;
+        this.limitDate = limitDate;
+        this.priority = priority;
+    }
+
+    public Task() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getLimitDate() {
+        return limitDate;
+    }
+
+    public void setLimitDate(Date limitDate) {
+        this.limitDate = limitDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", limitDate=" + limitDate +
+                ", priority='" + priority + '\'' +
+                ", employee=" + employee +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
